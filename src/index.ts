1e2fd54f2fa1659e37bfd783a58a5fa51ab79685
rektide <rektide+scm@voodoowarez.com>
2026-02-01 19:35:12 -0500

    chore: update package.json scripts and fix tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export { BlockInFile } from "./block-in-file.ts";
-export { defaultOptions, getDefaultOptions } from "./defaults.ts";
-export type { BlockInFileOptions, CreateArg, InputOptions, ParseResult } from "./types.ts";
-export { parseAndInsertBlock } from "./block-parser.ts";
-export { formatOutputs, generateDiff, writeDiff, writeOutput } from "./output.ts";
-export { createOpt, get, openInput, readInput } from "./input.ts";
+export { defaultOptions, getDefaultOptions } from "./defaults.ts"
+export type { BlockInFileOptions, CreateArg, InputOptions, ParseResult } from "./types.ts"
+export { parseAndInsertBlock } from "./block-parser.ts"
+export { formatOutputs, generateDiff, writeDiff, writeOutput } from "./output.ts"
+export { createOpt, get, openInput, readInput } from "./input.ts"
